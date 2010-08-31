9492e25b943f0913ccbb1523953033153fa64c06
Maarten Bezemer <Maarten Bezemer@localhost>
2010-08-31 18:01:37 +0200

    Added detection for 'unexpected/unknown' bits using the output log en grep Added code to monitor calibration functionality (without succes), probably same 'problem' as with motor temp: not all communication is made visible? Made opensource.c more flexible with a macro to specify whether the PHANTOM device is on node 0xffc0 or 0xffc1 (in order to keep logs compatible)

--- minimal-test/opensource.c
+++ minimal-test/opensource.c
@@ -28,6 +28,17 @@
 
 #include "libraw1394/raw1394.h"
 
+// Enable if PHANTOM ihas 0xffc1 as node id
+//#define USE_SECOND_NODE
+
+
+
+#ifndef USE_SECOND_NODE
+#define NODE 0xffc0
+#else
+#define NODE 0xffc1
+#endif
+
 quadlet_t writebuf[4] = { 0x07ff07ff, 0x53c007ff, 0x00000000, 0x00000000  };
 int readdata = 0;
 
@@ -58,11 +69,13 @@ int main()
   raw1394handle_t h1 = raw1394_new_handle_on_port(0);
   int nodes = raw1394_get_nodecount(h1);
   { quadlet_t buf = 0; raw1394_read(h1, 0xffc0, 0x1006000c, 4, &buf); }
+#ifdef  USE_SECOND_NODE
   { quadlet_t buf = 0; raw1394_read(h1, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h1, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h1, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h1, 0xffc1, 0x1006000c, 4, &buf); }
-  { quadlet_t buf = 0; raw1394_read(h1, 0xffc1, 0x10060010, 4, &buf); }
+#endif
+  { quadlet_t buf = 0; raw1394_read(h1, NODE, 0x10060010, 4, &buf); }
   raw1394_destroy_handle(h1);
   raw1394handle_t h2 = raw1394_new_handle();
   raw1394handle_t h3 = raw1394_new_handle();
@@ -73,11 +86,13 @@ int main()
   raw1394handle_t h6 = raw1394_new_handle_on_port(0);
   int nodes1 = raw1394_get_nodecount(h6);
   { quadlet_t buf = 0; raw1394_read(h6, 0xffc0, 0x1006000c, 4, &buf); }
+#ifdef  USE_SECOND_NODE
   { quadlet_t buf = 0; raw1394_read(h6, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h6, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h6, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h6, 0xffc1, 0x1006000c, 4, &buf); }
-  { quadlet_t buf = 0; raw1394_read(h6, 0xffc1, 0x10060010, 4, &buf); }
+#endif
+  { quadlet_t buf = 0; raw1394_read(h6, NODE, 0x10060010, 4, &buf); }
   raw1394_destroy_handle(h6);
   raw1394handle_t h7 = raw1394_new_handle();
   raw1394_get_port_info(h7, 0, 0);
@@ -85,11 +100,13 @@ int main()
   raw1394handle_t h8 = raw1394_new_handle_on_port(0);
   int nodes2 = raw1394_get_nodecount(h8);
   { quadlet_t buf = 0; raw1394_read(h8, 0xffc0, 0x1006000c, 4, &buf); }
+#ifdef  USE_SECOND_NODE
   { quadlet_t buf = 0; raw1394_read(h8, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h8, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h8, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h8, 0xffc1, 0x1006000c, 4, &buf); }
-  { quadlet_t buf = 0; raw1394_read(h8, 0xffc1, 0x10060010, 4, &buf); }
+#endif
+  { quadlet_t buf = 0; raw1394_read(h8, NODE, 0x10060010, 4, &buf); }
   raw1394_destroy_handle(h7);
   raw1394_set_port(h2, 0);
   raw1394_set_port(h3, 0);
@@ -98,18 +115,22 @@ int main()
   raw1394_get_fd(h3);
   int nodes3 = raw1394_get_nodecount(h2);
   { quadlet_t buf = 0; raw1394_read(h4, 0xffc0, 0x1006000c, 4, &buf); }
+#ifdef  USE_SECOND_NODE
   { quadlet_t buf = 0; raw1394_read(h4, 0xffc1, 0x1006000c, 4, &buf); }
+#endif
   raw1394handle_t h9 = raw1394_new_handle();
   raw1394_get_port_info(h9, 0, 0);
   raw1394_destroy_handle(h9);
   raw1394handle_t h10 = raw1394_new_handle_on_port(0);
   raw1394_get_nodecount(h10);
   { quadlet_t buf = 0; raw1394_read(h10, 0xffc0, 0x1006000c, 4, &buf); }
+#ifdef  USE_SECOND_NODE
   { quadlet_t buf = 0; raw1394_read(h10, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h10, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h10, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h10, 0xffc1, 0x1006000c, 4, &buf); }
-  { quadlet_t buf = 0; raw1394_read(h10, 0xffc1, 0x10060010, 4, &buf); }
+#endif
+  { quadlet_t buf = 0; raw1394_read(h10, NODE, 0x10060010, 4, &buf); }
   raw1394_destroy_handle(h10);
   raw1394_destroy_handle(h2);
   raw1394_destroy_handle(h3);
@@ -124,11 +145,13 @@ int main()
   raw1394handle_t h15 = raw1394_new_handle_on_port(0);
   raw1394_get_nodecount(h15);
   { quadlet_t buf = 0; raw1394_read(h15, 0xffc0, 0x1006000c, 4, &buf); }
+#ifdef  USE_SECOND_NODE
   { quadlet_t buf = 0; raw1394_read(h15, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h15, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h15, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h15, 0xffc1, 0x1006000c, 4, &buf); }
-  { quadlet_t buf = 0; raw1394_read(h15, 0xffc1, 0x10060010, 4, &buf); }
+#endif
+  { quadlet_t buf = 0; raw1394_read(h15, NODE, 0x10060010, 4, &buf); }
   raw1394_destroy_handle(h15);
   raw1394handle_t h16 = raw1394_new_handle();
   raw1394_get_port_info(h16, 0, 0);
@@ -136,11 +159,13 @@ int main()
   raw1394handle_t h17 = raw1394_new_handle_on_port(0);
   raw1394_get_nodecount(h17);
   { quadlet_t buf = 0; raw1394_read(h17, 0xffc0, 0x1006000c, 4, &buf); }
+#ifdef  USE_SECOND_NODE
   { quadlet_t buf = 0; raw1394_read(h17, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h17, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h17, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h17, 0xffc1, 0x1006000c, 4, &buf); }
-  { quadlet_t buf = 0; raw1394_read(h17, 0xffc1, 0x10060010, 4, &buf); }
+#endif
+  { quadlet_t buf = 0; raw1394_read(h17, NODE, 0x10060010, 4, &buf); }
   raw1394_destroy_handle(h17);
   raw1394_set_port(h11, 0);
   raw1394_set_port(h12, 0);
@@ -149,56 +174,62 @@ int main()
   int fd12 = raw1394_get_fd(h12);
   raw1394_get_nodecount(h11);
   { quadlet_t buf = 0; raw1394_read(h13, 0xffc0, 0x1006000c, 4, &buf); }
+#ifdef  USE_SECOND_NODE
   { quadlet_t buf = 0; raw1394_read(h13, 0xffc1, 0x1006000c, 4, &buf); }
+#endif
   raw1394handle_t h18 = raw1394_new_handle();
   raw1394_get_port_info(h18, 0, 0);
   raw1394_destroy_handle(h18);
   raw1394handle_t h19 = raw1394_new_handle_on_port(0);
   raw1394_get_nodecount(h19);
   { quadlet_t buf = 0; raw1394_read(h19, 0xffc0, 0x1006000c, 4, &buf); }
+#ifdef  USE_SECOND_NODE
   { quadlet_t buf = 0; raw1394_read(h19, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h19, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h19, 0xffc0, 0x1006000c, 4, &buf); }
   { quadlet_t buf = 0; raw1394_read(h19, 0xffc1, 0x1006000c, 4, &buf); }
-  { quadlet_t buf = 0; raw1394_read(h19, 0xffc1, 0x10060010, 4, &buf); }
+#endif
+  { quadlet_t buf = 0; raw1394_read(h19, NODE, 0x10060010, 4, &buf); }
   raw1394_destroy_handle(h19);
-  { unsigned char data = 0x01; raw1394_write(h13, 0xffc1, 0x1000, 1, (quadlet_t*) &data); }
+  { unsigned char data = 0x01; raw1394_write(h13, NODE, 0x1000, 1, (quadlet_t*) &data); }
 //      <- data: 0x01 
 
-  { unsigned char data = 0; raw1394_write(h13, 0xffc1, 0x1001, 1, (quadlet_t*) &data); }
+  { unsigned char data = 0; raw1394_write(h13, NODE, 0x1001, 1, (quadlet_t*) &data); }
 //      <- data: 0x00 
 
-  { quadlet_t data = 0xf80f0000; raw1394_write(h13, 0xffc1, 0x20010, 4, &data); }
+  { quadlet_t data = 0xf80f0000; raw1394_write(h13, NODE, 0x20010, 4, &data); }
 //      <- data: 0xf80f0000 
 
-  { char buffer; raw1394_read(h13, 0xffc1, 0x1001, 1, (quadlet_t*) &buffer); }
+  { char buffer; raw1394_read(h13, NODE, 0x1001, 1, (quadlet_t*) &buffer); }
 //      -> buffer: ??
 
-  { unsigned char data = 0x40; raw1394_write(h13, 0xffc1, 0x1001, 1, (quadlet_t*) &data); }
+  { unsigned char data = 0x40; raw1394_write(h13, NODE, 0x1001, 1, (quadlet_t*) &data); }
 //      <- data: 0x40
 
-  { char buffer = 0x40; raw1394_read(h13, 0xffc1, 0x1001, 1, (quadlet_t*) &buffer); }
+  { char buffer = 0x40; raw1394_read(h13, NODE, 0x1001, 1, (quadlet_t*) &buffer); }
 //      -> buffer: 0x40
 
-  { unsigned char data = 0; raw1394_write(h13, 0xffc1, 0x1001, 1, (quadlet_t*) &data); }
+  { unsigned char data = 0; raw1394_write(h13, NODE, 0x1001, 1, (quadlet_t*) &data); }
 //      <- data: ??
 
   printf("Found PHANTOM Omni.\n\n");
 
-  { char buffer; raw1394_read(h13, 0xffc1, 0x1083, 1, (quadlet_t*) &buffer); }
+  { char buffer; raw1394_read(h13, NODE, 0x1083, 1, (quadlet_t*) &buffer); }
 //      -> buffer: ??
 
-  { char buffer; raw1394_read(h13, 0xffc1, 0x1082, 1, (quadlet_t*) &buffer); }
+  { char buffer; raw1394_read(h13, NODE, 0x1082, 1, (quadlet_t*) &buffer); }
 //      -> buffer: ??
 
-  { unsigned char data = 0x08; raw1394_write(h13, 0xffc1, 0x1087, 1, (quadlet_t*) &data); }
+  { unsigned char data = 0x08; raw1394_write(h13, NODE, 0x1087, 1, (quadlet_t*) &data); }
 //      <- data: 0x08
 
   raw1394_iso_recv_init(h11, recv_handler, 1000, 64, 0, -1, 1);
   raw1394_iso_recv_start(h11, -1, -1, 0);
+  raw1394_set_bus_reset_handler(h11, 0);
 
   raw1394_iso_xmit_init(h12, 0 /*xmit_handler*/, 1000, 64, 1, 0, 1);
   raw1394_iso_xmit_start(h12, -1, -1);
+  raw1394_set_bus_reset_handler(h12, 0);
 
   while(!readdata)
   {
@@ -208,14 +239,17 @@ int main()
   }
 
 
-  { char buffer; raw1394_read(h13, 0xffc1, 0x1083, 1, (quadlet_t*) &buffer); }
+  { char buffer; raw1394_read(h13, NODE, 0x1083, 1, (quadlet_t*) &buffer); }
 //      -> buffer: ??
 
-  { char buffer; raw1394_read(h13, 0xffc1, 0x1082, 1, (quadlet_t*) &buffer); }
+  { char buffer; raw1394_read(h13, NODE, 0x1082, 1, (quadlet_t*) &buffer); }
 //      -> buffer: ??
 
-  { unsigned char data; raw1394_write(h13, 0xffc1, 0x1087, 1, (quadlet_t*) &data); }
+  { unsigned char data; raw1394_write(h13, NODE, 0x1087, 1, (quadlet_t*) &data); }
 //      <- data: ??
+
+  raw1394_set_bus_reset_handler(h11, 0);
+  raw1394_set_bus_reset_handler(h12, 0);
 
   raw1394_iso_shutdown(h12);
   raw1394_iso_shutdown(h11);
--- minimal-test/proprietary.c
+++ minimal-test/proprietary.c
@@ -5,17 +5,26 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include <HD/hd.h>
 #include <HDU/hduError.h>
 #include <HDU/hduVector.h>
 
-void PrintDevicePosition();
-HDCallbackCode ForceFeedbackCallback(void *pUserData);
-HDSchedulerHandle gCallbackHandle = HD_INVALID_HANDLE;
-
 // When enabled the proprietary.log does not match the opensource.log anymore...
-#define ADD_FORCEFEEDBACK
+//#define ADD_FORCEFEEDBACK
+//#define ADD_CALIBRATION
+//#define ADD_MOTORTEMP
+
+void PrintDevicePosition();
+#ifdef ADD_FORCEFEEDBACK
+HDCallbackCode ForceFeedbackCallback(void *pUserData);
+#endif
+HDSchedulerHandle gCallbackHandle = HD_INVALID_HANDLE;
+#ifdef ADD_CALIBRATION
+HDenum GetCalibrationStatus();
+HDboolean CheckCalibration(HDenum calibrationStyle);
+#endif
 
 static int count = 4;
 static int enableForceFeedback = 0;
@@ -24,11 +33,13 @@ int main(int argc, char* argv[])
 {
     HHD hHD;
     HDErrorInfo error;
+#ifdef ADD_CALIBRATION
     int supportedCalibrationStyles;
     int calibrationStyle;
-    
+#endif
+
     hHD = hdInitDevice(HD_DEFAULT_DEVICE);
-    if (HD_DEVICE_ERROR(error = hdGetError())) 
+    if (HD_DEVICE_ERROR(error = hdGetError()))
     {
         hduPrintError(stderr, &error, "Failed to initialize haptic device");
         fprintf(stderr, "\nPress any key to quit.\n");
@@ -37,6 +48,52 @@ int main(int argc, char* argv[])
 
     printf("Found %s.\n\n", hdGetString(HD_DEVICE_MODEL_TYPE));
 
+#ifdef ADD_CALIBRATION
+    hdGetIntegerv(HD_CALIBRATION_STYLE, &supportedCalibrationStyles);
+    if (supportedCalibrationStyles & HD_CALIBRATION_ENCODER_RESET)
+    {
+        calibrationStyle = HD_CALIBRATION_ENCODER_RESET;
+        printf("Calibration style: HD_CALIBRATION_ENCODER_RESET\n");
+    }
+    if (supportedCalibrationStyles & HD_CALIBRATION_INKWELL)
+    {
+        calibrationStyle = HD_CALIBRATION_INKWELL;
+        printf("Calibration style: HD_CALIBRATION_INKWELL\n");
+    }
+    if (supportedCalibrationStyles & HD_CALIBRATION_AUTO)
+    {
+        calibrationStyle = HD_CALIBRATION_AUTO;
+        printf("Calibration style: HD_CALIBRATION_AUTO\n");
+    }
+
+    /* Some haptic devices only support manual encoder calibration via a
+       hardware reset. This requires that the endpoint be placed at a known
+       physical location when the reset is commanded. For the PHANTOM haptic
+       devices, this means positioning the device so that all links are
+       orthogonal. Also, this reset is typically performed before the servoloop
+       is running, and only technically needs to be performed once after each
+       time the device is plugged in. */
+    if (calibrationStyle == HD_CALIBRATION_ENCODER_RESET)
+    {
+        printf("Please prepare for manual calibration by\n");
+        printf("placing the device at its reset position.\n\n");
+        printf("Press any key to continue...\n");
+
+        getch();
+
+        hdUpdateCalibration(calibrationStyle);
+        if (hdCheckCalibration() == HD_CALIBRATION_OK)
+        {
+            printf("Calibration complete.\n\n");
+        }
+        if (HD_DEVICE_ERROR(error = hdGetError()))
+        {
+            hduPrintError(stderr, &error, "Reset encoders reset failed.");
+            return -1;
+        }
+    }
+#endif
+
 #ifdef ADD_FORCEFEEDBACK
     gCallbackHandle = hdScheduleAsynchronous(ForceFeedbackCallback, 0, HD_MAX_SCHEDULER_PRIORITY);
 #endif
@@ -44,17 +101,41 @@ int main(int argc, char* argv[])
     if (HD_DEVICE_ERROR(error = hdGetError()))
     {
         hduPrintError(stderr, &error, "Failed to start the scheduler");
-        return -1;           
+        return -1;
     }
+
+#ifdef ADD_CALIBRATION
+    /* Some haptic devices are calibrated when the gimbal is placed into
+       the device inkwell and updateCalibration is called.  This form of
+       calibration is always performed after the servoloop has started
+       running. */
+    if (calibrationStyle  == HD_CALIBRATION_INKWELL)
+    {
+        if (GetCalibrationStatus() == HD_CALIBRATION_NEEDS_MANUAL_INPUT)
+        {
+            printf("Please place the device into the inkwell ");
+            printf("for calibration.\n\n");
+        }
+    }
+#endif
 
     while(count > 0)
     {
       PrintDevicePosition();
+#ifdef ADD_CALIBRATION
+      printf("\n\nstart hgUpdateCalibration()\n");
+      CheckCalibration(calibrationStyle);
+      printf("done hgUpdateCalibration()\n\n\n");
+      count--;
+#endif
 #ifndef ADD_FORCEFEEDBACK
-      count = 0;
+#ifndef ADD_CALIBRATION
+      count--;
+#endif
 #endif
     }
-    
+    PrintDevicePosition();
+
     hdStopScheduler();
 #ifdef ADD_FORCEFEEDBACK
     hdUnschedule(gCallbackHandle);
@@ -78,32 +159,113 @@ HDCallbackCode DevicePositionCallback(void *pUserData)
 void PrintDevicePosition()
 {
     hduVector3Dd position;
-    HDint numMotors;
-    HDdouble *temps;
-    int i;
 
     hdScheduleSynchronous(DevicePositionCallback, position,
         HD_DEFAULT_SCHEDULER_PRIORITY);
-        
-    printf("Device position: %.3f %.3f %.3f\n", 
+
+    printf("Device position: %.3f %.3f %.3f\n\n\n",
         position[0], position[1], position[2]);
 
+#ifdef ADD_MOTORTEMP
+    HDint numMotors;
+    HDdouble *temps;
+    int i;
     // TODO Nothing seems to happen when the temperatures are read... (missing TRACEs in libraw1394??)
+    printf("\n\nstart reading motor temp\n");
     hdGetIntegerv(HD_OUTPUT_DOF, &numMotors);
     temps = (HDdouble *) malloc(sizeof(HDdouble) * numMotors);
     hdGetDoublev(HD_MOTOR_TEMPERATURE, temps);
+    printf("\n\nfinished reading motor temp\n");
 
     for(i = 0; i < numMotors; i++)
       printf("Motor %d temp: %f\n", i, temps[i]);
     free(temps);
+#endif
 }
 
+#ifdef ADD_CALIBRATION
+HDCallbackCode HDCALLBACK UpdateCalibrationCallback(void *pUserData)
+{
+    HDenum *calibrationStyle = (int *) pUserData;
+
+    if (hdCheckCalibration() == HD_CALIBRATION_NEEDS_UPDATE)
+    {
+        printf("hdUpdateCalibration called...\n");
+        hdUpdateCalibration(*calibrationStyle);
+    }
+
+    return HD_CALLBACK_DONE;
+}
+
+HDCallbackCode HDCALLBACK CalibrationStatusCallback(void *pUserData)
+{
+    HDenum *pStatus = (HDenum *) pUserData;
+    printf("CalibrationStatusCallback()\n");
+
+    hdBeginFrame(hdGetCurrentDevice());
+    *pStatus = hdCheckCalibration();
+    hdEndFrame(hdGetCurrentDevice());
+
+    printf("CalibrationStatusCallback() ended\n");
+    return HD_CALLBACK_DONE;
+}
+
+HDenum GetCalibrationStatus()
+{
+    HDenum status;
+    hdScheduleSynchronous(CalibrationStatusCallback, &status,
+                          HD_DEFAULT_SCHEDULER_PRIORITY);
+    return status;
+}
+
+HDboolean CheckCalibration(HDenum calibrationStyle)
+{
+    HDenum status = GetCalibrationStatus();
+
+    printf("CheckCalibration() start...\n");
+
+    if (status == HD_CALIBRATION_OK)
+    {
+        return HD_TRUE;
+    }
+    else if (status == HD_CALIBRATION_NEEDS_MANUAL_INPUT)
+    {
+        printf("Calibration requires manual input...\n");
+        return HD_FALSE;
+    }
+    else if (status == HD_CALIBRATION_NEEDS_UPDATE)
+    {
+        hdScheduleSynchronous(UpdateCalibrationCallback, &calibrationStyle,
+            HD_DEFAULT_SCHEDULER_PRIORITY);
+
+        if (HD_DEVICE_ERROR(hdGetError()))
+        {
+            printf("\nFailed to update calibration.\n\n");
+            return HD_FALSE;
+        }
+        else
+        {
+            printf("\nCalibration updated successfully.\n\n");
+            count = 0;
+            return HD_TRUE;
+        }
+    }
+    else
+    {
+//        assert(!"Unknown calibration status");
+        return HD_FALSE;
+    }
+}
+#endif
+
+#ifdef ADD_FORCEFEEDBACK
 HDCallbackCode ForceFeedbackCallback(void *pUserData)
 {
     long int force[3];
     int i;
 
-    printf("Motors %d\n", enableForceFeedback);
+    hdBeginFrame(hdGetCurrentDevice());
+    printf("Motors enabled: %s\n", enableForceFeedback ? "yes" : "no");
     for(i = 0; i < 3; i++)
       force[i] = (enableForceFeedback?0x1234:0);
     if(enableForceFeedback)
@@ -113,10 +275,9 @@ HDCallbackCode ForceFeedbackCallback(void *pUserData)
     enableForceFeedback = !enableForceFeedback;
     count--;
 
-    hdBeginFrame(hdGetCurrentDevice());
-    //hdSetLongv(HD_CURRENT_MOTOR_DAC_VALUES, force);
+    hdSetLongv(HD_CURRENT_MOTOR_DAC_VALUES, force);
     hdEndFrame(hdGetCurrentDevice());
 
     return HD_CALLBACK_CONTINUE;
 }
-
+#endif
